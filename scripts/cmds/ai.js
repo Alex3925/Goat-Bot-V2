4a9e62ff18768fe4f2164b9f13f7ff79dca72921
Xyryllpanget11 <93780391+Xyryllpanget11@users.noreply.github.com>
2024-04-29 14:43:19 +0800

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -21,7 +21,7 @@ module.exports = {
     const prompt = args.join(" ");
 
     try {
-      const res = await axios.get(`https://apis-dalle-gen.onrender.com/dalle3?auth_cookie_U=${_U}&auth_cookie_KievRPSSecAuth=${KievRPSSecAuth}&prompt=${encodeURIComponent(prompt)}`);
+      const res = await axios.get(`https://apis-dalle-gen.onrender.com/dalle3?g.a000jAiTJOlfCg8Zh0hMjcH0_SwLRpbf9VUQskIpXMEtaBwdfq-O0iTKOSpj7DNL1zCLJ_3YkAACgYKATgSAQASFQHGX2MimEQ2KuUX8Tubz9MHIclxExoVAUF8yKrAiZcNqmxAbXKcAB8VIbWV0076_U=${_U}&g.a000jAiTJOlfCg8Zh0hMjcH0_SwLRpbf9VUQskIpXMEtaBwdfq-O0iTKOSpj7DNL1zCLJ_3YkAACgYKATgSAQASFQHGX2MimEQ2KuUX8Tubz9MHIclxExoVAUF8yKrAiZcNqmxAbXKcAB8VIbWV0076_KievRPSSecAuth=${KievRPSSecAuth}&prompt=${encodeURIComponent(prompt)}`);
       const data = res.data.results.images;
 
       if (!data || data.length === 0) {
